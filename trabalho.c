7c198083031bd5f1b129a05f3586bc0c323ed20d
Radajaaj <1vitor.camargo@gmail.com>
2025-03-18 22:35:57 -0300

    Separadas as structs

--- trabalho.c
+++ trabalho.c
@@ -1,49 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-struct entrada_arquivo{
-    char status;
-    char nome[12];
-    char ext[4];
-    char tipo;
-    unsigned int primeiro_bloco;
-    unsigned int tamanho;
-    unsigned int numero_blocos_usados;
-};
-
-struct boot_record{
-    unsigned short bytes_por_bloco;          //2 bytes
-    unsigned short blocos_reservados;        //2 bytes
-    unsigned int num_blocos_livres;          //4 bytes
-    unsigned int num_blocos_tabela_entradas; //...
-    unsigned int num_blocos_secao_dados;
-    unsigned int num_blocos_totais;
-    unsigned int num_blocos_reservados_raiz;
-    unsigned int quant_entradas_sistema;
-    int padding;                             //4 bytes para completar
-};
-
-struct entrada_diretorio{
-    char status;   //apenas um byte para status
-    char nome[12];
-    char ext[4];
-    char tipo;
-    unsigned int primeiro_bloco;
-    unsigned int tamanho;
-    unsigned int numero_blocos_usados;
-    unsigned short padding;
-};
+#include "structs.c"
 
 
-struct Bloco{
-    struct Bloco *prox;
-	char conteudo[508];
-};
-
-
-
-typedef struct Bloco bloco;
-typedef bloco* LISTA; //aponta para o inicio da lista;
 
 
 LISTA* cria_lista(){
